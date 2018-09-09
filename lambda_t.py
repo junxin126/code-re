398b3631266d3d286c312b000ff3e912a84ab920
junxin126 <11332400@qq.com>
2018-09-09 15:54:03 +0800

    lambda

--- lambda_t.py
+++ lambda_t.py
@@ -5,5 +5,5 @@ import re
 
 # 将https://github.com/junxin126/code-re中的https://github.com/提取出来
 text = "https://github.com/junxin126/code-re"
-result = re.sub(r"(https://.*?/).*", lambda x: x.group(1), text)
+result = re.sub(r"(http.*?m/).*", lambda x: x.group(1), text)
 print(result)
--- note.py
+++ note.py
@@ -1,10 +1,10 @@
 # coding:utf-8
 import re
 
-# 在“*”或者“+”后面加“？”即可关闭贪婪模式
-result = re.match(r"(.*?)(\d*?)(yes)","thisnumber234yes")
-gr_re=result.groups()
-print (result)
-print (gr_re)
+# 在“*”或者“+”后面加“？”即可关闭贪婪模式，实际上“?"表示{0，1}，取0至1
+result = re.match(r"(.+?)(\d+)", "this number is 234")
+print result.groups()
 
-
+# 匹配整数
+result = re.match("-?\d+$", "8888")
+print result.group()
