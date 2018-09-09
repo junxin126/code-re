0668a41b58349742c0483bc5f0ffc784c4c73f76
junxin126 <11332400@qq.com>
2018-09-09 15:06:28 +0800

    lambda

---
+++ lambda_t.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+
+import re
+
+
+# 将https://github.com/junxin126/code-re中的https://github.com/提取出来
+text = "https://github.com/junxin126/code-re"
+result = re.sub(r"(https://.*?/).*", lambda x: x.group(1), text)
+print(result)
